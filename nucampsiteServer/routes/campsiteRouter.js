1b742b3eb92964067e9e115938d249aa4b0d1261
Davis G. Trent <davis@davistrent.com>
2020-03-26 20:56:57 -0700

    Exercise - Mongoose Population.

--- nucampsiteServer/routes/campsiteRouter.js
+++ nucampsiteServer/routes/campsiteRouter.js
@@ -11,6 +11,7 @@ campsiteRouter.use(bodyParser.json());
 campsiteRouter.route('/')
 .get((req, res, next) => {
     Campsite.find()
+    .populate('comments.author')
     .then(campsites => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
@@ -46,6 +47,7 @@ campsiteRouter.route('/')
 campsiteRouter.route('/:campsiteId')
 .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+    .populate('comments.author')
     .then(campsite => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
@@ -81,6 +83,7 @@ campsiteRouter.route('/:campsiteId')
 campsiteRouter.route('/:campsiteId/comments')
 .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+    .populate('comments.author')
     .then(campsite => {
         if(campsite) {
             res.statusCode = 200;
@@ -98,6 +101,7 @@ campsiteRouter.route('/:campsiteId/comments')
     Campsite.findById(req.params.campsiteId)
     .then(campsite => {
         if(campsite) {
+            req.body.author = req.user._id;
             campsite.comments.push(req.body);
             campsite.save()
             .then(campsite => {
@@ -142,44 +146,10 @@ campsiteRouter.route('/:campsiteId/comments')
     .catch(err => next(err));
 });
 
-campsiteRouter.route('/:campsiteId')
-.get((req, res, next) => {
-    Campsite.findById(req.params.campsiteId)
-    .then(campsite => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(campsite);
-    })
-    .catch(err => next(err));
-})
-.post(authenticate.verifyUser, (req, res) => {
-  res.statusCode = 403;
-    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
-})
-.put(authenticate.verifyUser, (req, res, next) => {
-   Campsite.findByIdAndUpdate(req.params.campsiteId, {
-       $set: req.body,
-   }, { new: true })
-   .then(campsite => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(campsite);
-   })
-   .catch(err => next(err));
-})
-.delete(authenticate.verifyUser, (req, res, next) => {
-    Campsite.findByIdAndDelete(req.params.campsiteId)
-    .then(response => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(response);
-   })
-   .catch(err => next(err));
-});
-
 campsiteRouter.route('/:campsiteId/comments/:commentId')
 .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+    .populate('comments.author')
     .then(campsite => {
         if(campsite && campsite.comments.id(req.params.commentId)) {
             res.statusCode = 200;
